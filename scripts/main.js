cd0cd5b54a13a158de3b0fc9843a69de3f2111a7
shaishabcoding <myshaishab@gmail.com>
2024-01-16 18:44:22 +0600

    added unique section

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 let isMenuOpened = false;
 const menu = document.getElementById("menu");
 
-function toggleOpenMenu() {
-  if (isMenuOpened)
-    menu.style.display = "flex"
-  else
-     menu.style.display = "none"
+function toggleOpenMenu(el) {
+  if (!isMenuOpened) {
+    menu.style.display = "flex";
+    el.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  } else {
+    menu.style.display = "none";
+    el.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
   isMenuOpened = !isMenuOpened;
 }
